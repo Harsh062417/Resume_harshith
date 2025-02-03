2ea9dd8d6e536819fc42217f698e88e1e279c8a1
Harsh062417 <harshith062417@gmail.com>
2025-02-03 18:21:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ particlesJS.load('particles-js', 'particles.json', function() {
     console.log('Particles.js loaded!');
   });
 
-  // Set up the scene, camera, and renderer
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ alpha: true }); // Alpha for transparency
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
 // Create a 3D object (e.g., a cube)
 const geometry = new THREE.BoxGeometry();
